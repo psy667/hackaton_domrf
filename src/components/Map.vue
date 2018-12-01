7be5fec1e5dbc48581579fdcfae697a27049c5af
psy667 <arseniyoguzov@gmail.com>
2018-12-01 13:42:24 +0300

    отображение маркеров циклом

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -5,43 +5,24 @@
                 zoom="10"
                 style="width: 100%; height: 600px;"
                 :cluster-options="{
-    1: {clusterDisableClickZoom: true}
+    1: {clusterDisableClickZoom: false}
   }"
-                :behaviors="['ruler']"
-                :controls="['trafficControl']"
+                :behaviors="['default']"
+                :controls="['searchControl', 'typeSelector']"
                 :placemarks="placemarks"
                 map-type="map"
                 @map-was-initialized="initHandler"
         >
-            <ymap-marker
-                    marker-id="1"
+            <ymap-marker v-for='marker in markers'
+                    marker-id="marker.id"
                     marker-type="placemark"
-                    :coords="[54.7, 39.7]"
-                    hint-content="Hint content 1"
-                    :balloon="{header: 'header', body: 'body', footer: 'footer'}"
-                    :icon="{color: 'green', glyph: 'cinema'}"
-                    cluster-name="1"
-            ></ymap-marker>
-            <ymap-marker
-                    marker-id="2"
-                    marker-type="placemark"
-                    :coords="[54.6, 39.8]"
+                    :coords="marker.coords"
                     hint-content="Hint content 1"
                     :balloon="{header: 'header', body: 'body', footer: 'footer'}"
                     :icon="{color: 'green', glyph: 'cinema'}"
                     cluster-name="1"
             ></ymap-marker>
 
-            <ymap-marker
-                    marker-id="3"
-                    marker-type="circle"
-                    :coords="[54.62896654088406, 39.731893822753904]"
-                    circle-radius="1600"
-                    hint-content="Hint content 1"
-                    :marker-fill="{color: '#000000', opacity: 0.4}"
-                    :marker-stroke="{color: '#ff0000', width: 5}"
-                    :balloon="{header: 'header', body: 'body', footer: 'footer'}"
-            ></ymap-marker>
 
         </yandex-map>
     </div>
