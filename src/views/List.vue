d48042a8df7caf2a07392d28d8f0bbd2dfe3af02
Zenderg <danil54658798@gmail.com>
2018-12-01 17:29:10 +0300

    create second page

---
+++ src/views/List.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="list">
+    <div v-for="marker in markers" :key="marker.id">
+      <pre>{{ marker }}</pre>
+    </div>
+  </div>
+</template>
+
+<script>
+    import {mapState} from 'vuex'
+
+  export default{
+      name:"list",
+      data(){
+          return{
+
+          }
+      },
+      computed: {
+          ...mapState({
+              markers: state => state.markers
+          })
+      }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
