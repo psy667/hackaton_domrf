5130b1d96d145e7a72ccfdacbdefcccd2299d2b8
psy667 <arseniyoguzov@gmail.com>
2018-12-01 14:46:55 +0300

    добавлен сайдбар

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <div class="container">
+    <h3>{{ obj.name }}</h3>
+    <div class="adress">
+      Адрес: {{ obj.adress }}
+    </div>
+    <div class="cost">
+      Стоимость: {{ obj.cost }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+  data(){
+    return {
+      obj: {
+        name: '',
+        adress: '',
+        coords: '',
+        cost: ''
+      }
+    };
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .container{
+    width: 200px;
+    height: 500px;
+  }
+</style>
