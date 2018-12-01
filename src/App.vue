65317000ba54b8f669f660b433acfe4a7d206cff
psy667 <arseniyoguzov@gmail.com>
2018-12-01 19:52:36 +0300

    added list

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">Aboutjnkывмфк</router-link>
+    <div id="nav" class='tab tab-block'>
+      <router-link to="/" class='tab-item'>Карта</router-link>
+      <router-link to="/list" class='tab-item'>Список</router-link>
     </div>
+
     <router-view/>
   </div>
 </template>
@@ -16,13 +17,36 @@
   text-align: center;
   color: #2c3e50;
 }
+.tab{
+    max-width: 500px;
+    margin: auto !important;
+}
 #nav {
   padding: 30px;
+  padding-top: 0;
+  padding-bottom: 10px;
+  border-bottom: 0;
+
   a {
-    font-weight: bold;
+
+    padding-top: 20px;
+    // font-weight: bold;
+
+    border-bottom: 1px solid;
+    border-bottom-color: #eee;
     color: #2c3e50;
+    text-decoration: none;
+    padding-bottom: 5px;
+    &:focus{
+      outline: 0!important;
+    }
     &.router-link-exact-active {
-      color: #42b983;
+      border-bottom-color: #5755d9;
+      border-bottom: 2px solid;
+      color: #5755d9;
+
+      width: 100%;
+      display: block;
     }
   }
 }
--- src/views/List.vue
+++ src/views/List.vue
@@ -1,19 +1,38 @@
 <template>
-  <div class="list">
-    <div v-for="marker in markers" :key="marker.id">
-      <pre>{{ marker }}</pre>
+  <div class="">
+
+
+    <div class="card" v-for='obj in markers'>
+      <div class="card-image">
+        <img src="https://0.pik.ru.cdn.pik-service.ru/specialoffers/0/180/6_36b545a33d449e9fee28a4779f63b6d6.jpg" class="img-responsive">
+      </div>
+      <div class="card-body">
+        <div class="block">
+          <div class="card-title h3">{{ obj.name }}</div>
+          <div class="card-title h4">{{ obj.cost }} руб.</div>
+        </div>
+        <div class="block">
+          <div class="text-gray">{{ obj.adress }}</div>
+          <div class="buttons">
+            <button class="btn btn-primary">На страницу объекта</button>
+            <button class="btn">Подробнее<i class="icon icon-arrow-down"></i></button>
+          </div>
+        </div>
+
+
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-    import {mapState} from 'vuex'
+  import {mapState} from 'vuex'
 
   export default{
       name:"list",
       data(){
           return{
-
+            text: "hello"
           }
       },
       computed: {
@@ -25,5 +44,33 @@
 </script>
 
 <style lang="scss">
+  .card{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
 
+    max-width: 960px;
+    margin: auto;
+    margin-bottom: 40px;
+    border-radius: 5px;
+    box-shadow: 0 3px 20px -5px #0003;
+    .card-image{
+      width: 30%;
+      // min-height: 200px;
+    }
+    .card-body{
+      width: 70%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      .block{
+        display: flex;
+        justify-content: space-between;
+      }
+      .buttons button:first-child{
+        margin-right: 10px;
+      }
+    }
+
+  }
 </style>
